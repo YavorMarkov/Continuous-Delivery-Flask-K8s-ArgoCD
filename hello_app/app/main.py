354b58521c3bdb3ef313da0e5b992f0f2ce99e8f
Yavor Markov <yavormaxpower@gmail.com>
2023-02-03 18:29:41 +0200

    Change image version

--- hello_app/app/main.py
+++ hello_app/app/main.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def msg():
     return "Hello World! Deployed using Argo CD!"
 
+@app.route('/')
+def msg('/around'):
+def argocd_msg():
+    return("Isn't Argo CD pretty amazing?") 
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
